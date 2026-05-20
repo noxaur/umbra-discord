5694adb1b871d18c60b893b89c89c9da7211f0ce
nox <nox.heights@gmail.com>
2026-05-20 16:36:28 +0200

    feat: add Discord markdown to HTML converter

---
+++ src/core/formatting/discord_markdown.h
@@ -0,0 +1,29 @@
+#ifndef DISCORD_MARKDOWN_H
+#define DISCORD_MARKDOWN_H
+
+#include <QString>
+
+class DiscordMarkdown {
+public:
+    static QString toHtml(const QString &markdown);
+
+private:
+    static QString escapeHtml(const QString &text);
+    static QString processInline(const QString &text);
+    static QString processCodeBlocks(const QString &text);
+    static QString processBlockquotes(const QString &text);
+    static QString processLists(const QString &text);
+    static QString processInlineCode(const QString &text);
+    static QString processBold(const QString &text);
+    static QString processItalic(const QString &text);
+    static QString processUnderline(const QString &text);
+    static QString processStrikethrough(const QString &text);
+    static QString processSpoilers(const QString &text);
+    static QString processLinks(const QString &text);
+    static QString processMentions(const QString &text);
+    static QString processTimestamps(const QString &text);
+    static QString formatDuration(qint64 seconds);
+    static QString processAutoLinks(const QString &text);
+};
+
+#endif // DISCORD_MARKDOWN_H
